a8ba3d65070721139048ac38e4bd07b567e3f0bd
Henry Smialowicz <henry.smialowicz@gmail.com>
2026-01-23 16:05:27 -0500

    Data Deconstruction

--- sniffer.py
+++ sniffer.py
@@ -5,3 +5,31 @@ import socket
 import struct
 import sys
 import argparse
+
+parser = argparse.ArgumentParser(description='Network packet sniffer')
+parser.add_argument('--ip', help='IP address to sniff on', required=True)
+opts = parser.parse_args()
+
+
+class Packet:
+    def __init__(self, data):
+        self.packet = data
+        header = struct.unpack('<BBHHHBBH4s4s', self.packet[0:20])
+        self.ver = header[0] >> 4
+        self.ihl = header[0] & 0xF
+        self.tos = header[1]
+        self.len = header[3]
+        self.off = header[4]
+        self.ttl = header[5]
+        self.pro = header[6]
+        self.num = header[7]
+        self.src = header[8]
+        self.dst = header[9]
+
+
+def sniff():
+    pass
+
+
+if __name__ == '__main__':
+    sniff()
